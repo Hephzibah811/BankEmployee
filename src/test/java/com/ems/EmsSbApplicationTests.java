7032d63bba7f423c62cebe9007b5592fdcf84822
Hephzibah S <142471663+Hephzibah811@users.noreply.github.com>
2024-02-10 13:54:10 +0530

    Update EmsSbApplicationTests.java

--- src/test/java/com/ems/EmsSbApplicationTests.java
+++ src/test/java/com/ems/EmsSbApplicationTests.java
@@ -1,33 +1,33 @@
-package com.ems;
+// package com.ems;
 
-import static org.junit.Assert.*;
+// import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-import com.ems.bean.Employee;
-import com.ems.dao.EmployeeDAO;
+// import com.ems.bean.Employee;
+// import com.ems.dao.EmployeeDAO;
 
 
-@SpringBootTest
- class EmsSbApplicationTest {
+// @SpringBootTest
+//  class EmsSbApplicationTest {
     
-    @Autowired
-    static EmployeeDAO dao;
-    Employee bean = new Employee(101,"hephzi","sebastian","Trainee","11953","hephzi11953rz");
+//     @Autowired
+//     static EmployeeDAO dao;
+//     Employee bean = new Employee(101,"hephzi","sebastian","Trainee","11953","hephzi11953rz");
 
-    @Test
-    void contextLoads() {
+//     @Test
+//     void contextLoads() {
         
-    }
+//     }
     
-    @Test
-    public void testperformInsert() {
-        dao = new EmployeeDAO();
-        boolean value = dao.insert(bean);
-        assertEquals(value,true);
-    }
+//     @Test
+//     public void testperformInsert() {
+//         dao = new EmployeeDAO();
+//         boolean value = dao.insert(bean);
+//         assertEquals(value,true);
+//     }
 
 
-}
+// }
